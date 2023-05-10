10d66d088759bf8665eba49df312c749d9b3140d
NamHyeong <86337357+NamNaam@users.noreply.github.com>
2023-05-10 15:40:56 +0900

    [Silver IV] Title: 카드 놓기, Time: 40 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,39 +1,14 @@
+from itertools import permutations
+
 n = int(input())
 k = int(input())
 card = []
-num = []
+num = set()
 
 for _ in range(n):
     card.append(input())
 
-if k == 2:
-    for i in range(n):
-        for j in range(n):
-            if j == i:
-                continue
-            x = card[i]+ card[j]
-            if not x in num:
-                num.append(x)
-
-elif k == 3:
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                if j == i or j == k or k == i:
-                    continue
-                x = card[i]+ card[j] + card[k]
-                if not x in num:
-                    num.append(x)
-
-else:
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                for l in range(n):
-                    if j == i or j == k or k == i or j == l or k == l or i == l:
-                        continue
-                    x = card[i]+ card[j] + card[k] + card[l]
-                    if not x in num:
-                        num.append(x)
+for per in permutations(card, k):
+    num.add(''.join(per))
 
 print(len(num))
